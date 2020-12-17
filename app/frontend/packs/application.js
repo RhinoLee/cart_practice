57695fc7132c8fc5637dbdcfa4c4d24a5953bdeb
RhinoLee <picklerhino@gmail.com>
2020-12-17 16:48:14 +0800

    header & footer

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,6 +6,9 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
 import 'scripts/shared'
 import 'scripts/frontend';
 
