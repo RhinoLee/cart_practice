edaf31e915d7c22bd4f1bb7a99e269f5a0a84494
RhinoLee <picklerhino@gmail.com>
2021-01-06 16:36:29 +0800

    add time spec for chrismas

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -16,6 +16,23 @@ class Cart
 
   end
 
+  def cart_total_price 
+    @items.reduce(0) { |sum, item| sum + item.total_price }
+  end
+
+  def christmas(total_price)
+    
+    Timecop.freeze(Time.zone.local(2000,12,25)) do
+      currentMonth = Time.now.month
+      currentDay = Time.now.day
+
+      if currentMonth == 12 && currentDay == 25 
+        total_price.to_i - 100
+      end
+    end
+    
+  end
+
   def empty? 
     @items.empty?
   end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   describe "基本功能" do 
+
     it "可把商品丟進購物車，購物車就會出現該商品" do 
       cart = Cart.new
       cart.add_item(2)
@@ -28,6 +29,33 @@ RSpec.describe Cart, type: :model do
       expect(cart.items.first.product).to be_a Product
 
     end
+
+    it "可以計算整台購物車的總消費金額" do 
+      cart = Cart.new 
+
+      p1 = create(:product, sell_price: 5)
+      p2 = create(:product, sell_price: 10)
+
+      3.times { cart.add_item(p1.id) }
+      2.times { cart.add_item(p2.id) }
+
+      expect(cart.cart_total_price).to eq(35)
+
+    end
+
+    it "聖誕節折扣，滿千送百" do 
+      cart = Cart.new 
+
+      p1 = create(:product, sell_price: 1000)
+      p2 = create(:product, sell_price: 1000)
+
+      cart.add_item(p1.id)
+      cart.add_item(p2.id)
+      
+      expect(cart.christmas(cart.cart_total_price)).to eq(1900)
+    end
+
+    
 
   end
 
